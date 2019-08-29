01d3275998178840b9451b4c09b0af15dd22baab
Sergey111991KV <sergey.cosilov@gmail.com>
2019-08-29 12:30:47 +0300

    gg

---
+++ GazpromAzs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GazpromAzs
+//
+//  Created by Сергей Косилов on 29.08.2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
